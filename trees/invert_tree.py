4ccc5e8e188832bd82a0eac57fb67f2df25e609c
ajangid007 <anuradha.jangid@gmail.com>
2024-01-03 21:50:26 -0800

    updates for invert tree

--- trees/invert_tree.py
+++ trees/invert_tree.py
@@ -87,7 +87,13 @@ class Tree:
 
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        pass
+        if root is not None:
+            temp = root.left
+            root.left = root.right
+            root.right = temp
+            self.invertTree(root.left)
+            self.invertTree(root.right)
+        return root
 
 
 head = Tree()
