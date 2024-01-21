d1063c33eb6ea96a77fe91d0cc6cbd1779897cb9
ajangid007 <anuradha.jangid@gmail.com>
2024-01-21 13:25:56 -0800

    removes redundant code

--- tries/search_words.py
+++ tries/search_words.py
@@ -5,39 +5,6 @@ class TrieNode:
         self.children = {}
         self.is_word = False
 
-class Trie:
-
-    def __init__(self):
-        self.root = TrieNode()
-        
-
-    def insert(self, word: str) -> None:
-        node = self.root
-        for char in word:
-            if not char in node.children:
-                node.children[char] = TrieNode()
-            node = node.children[char]
-        node.is_word = True
-        
-
-    def search(self, word: str) -> bool:
-        node = self.root
-        for char in word:
-            if not char in node.children:
-                return False
-            node = node.children[char]
-        return node.is_word
-        
-
-    def startsWith(self, prefix: str) -> bool:
-        node = self.root
-        for char in prefix:
-            if not char in node.children:
-                return False
-            node = node.children[char]
-        return True
-
-
 class WordDictionary:
     def __init__(self) -> None:
         self.root = TrieNode()
