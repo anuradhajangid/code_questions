cf76c02e505e3397fe9a8c20289cf82f48e04e96
ajangid007 <anuradha.jangid@gmail.com>
2024-02-16 13:34:10 -0800

    less optimized solution

---
+++ backtracking/combination_sum2.py
@@ -0,0 +1,29 @@
+#https://leetcode.com/problems/combination-sum/
+class Solution(object):
+    def combinationSum2(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        result = []
+        candidates = sorted(candidates)
+        self.backtrack(0,candidates, target, [], result)
+        return result
+    
+    def backtrack(self, index, nums, target, path, result):
+        if target < 0:
+            return 
+        if target == 0 and path not in result:
+            result.append(path)
+            return 
+        for i in range(index,len(nums)):
+            self.backtrack(i+1, nums, target-nums[i], path+[nums[i]], result)
+        
+
+        
+
+
+s = Solution()
+assert s.combinationSum2([10,1,2,7,6,1,5], 8) == [[1,1,6],[1,2,5],[1,7],[2,6]]
+assert s.combinationSum2([2,5,2,1,2], 5) == [[1,2,2],[5]]
