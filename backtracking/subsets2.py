568e663bb47ab1461ba0f1a3e9f6d3d3552f5279
ajangid007 <anuradha.jangid@gmail.com>
2024-02-16 13:03:39 -0800

    subset2 optimized

--- backtracking/subsets2.py
+++ backtracking/subsets2.py
@@ -1,7 +1,6 @@
 #https://leetcode.com/problems/subsets-ii/
-
 class Solution(object):
-    def subsetsWithDup(self, nums):
+    def subsetsWithDupLessOptimized(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
@@ -14,6 +13,24 @@ class Solution(object):
             for i in range(len(indexednums)):
                 backtrack(indexednums[i+1:], current+[indexednums[i]], result)
         backtrack(nums, [], result)
+        return result
+    
+    def subsetsWithDup(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        result = []
+        nums = sorted(nums)
+        def backtrack(index, current):
+            result.append(list(current))
+            for i in range(index, len(nums)):
+                if i != index and nums[i] == nums[i-1]:
+                    continue
+                current.append(nums[i])
+                backtrack(i+1, current)
+                current.pop()
+        backtrack(0, [])
         return result
 
 s = Solution()
