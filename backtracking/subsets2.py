bc87ff86e12163f1c284140b9745739754d0041b
ajangid007 <anuradha.jangid@gmail.com>
2024-02-16 12:56:03 -0800

    subset solution1

--- backtracking/subsets2.py
+++ backtracking/subsets2.py
@@ -1,17 +1,20 @@
-#https://leetcode.com/problems/subsets/description/
+#https://leetcode.com/problems/subsets-ii/
+
 class Solution(object):
-    def subsets(self, nums):
+    def subsetsWithDup(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
         """
         result = []
         def backtrack(indexednums, current, result):
-            result.append(current)
+            if not current in result:
+                result.append(current)
             for i in range(len(indexednums)):
                 backtrack(indexednums[i+1:], current+[indexednums[i]], result)
         backtrack(nums, [], result)
         return result
-    
+
 s = Solution()
-assert s.subsets([1,2,3]) == [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+#[[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+assert s.subsetsWithDup([1,2,2]) == [[],[1],[1,2],[1,2,2],[2],[2,2]]
