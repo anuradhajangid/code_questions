bbd1d66c9857fc1331d0a2a26ecd692a6e8d7c0f
ajangid007 <anuradha.jangid@gmail.com>
2024-02-20 13:49:13 -0800

    digraph order

---
+++ graph/algo.digraph_order.py
@@ -0,0 +1,127 @@
+from collections import defaultdict
+from collections import deque
+
+
+class Digraph:
+    def __init__(self) -> None:
+        self.graph = defaultdict(list)
+        self.e = []
+    
+    @property
+    def V(self):
+        return list(self.graph.keys())
+    
+    @property
+    def E(self):
+        return self.e
+
+    def addEdge(self, fr:int, to:int) -> None:
+        if to:
+            self.graph[fr].append(to)
+            self.E.append((fr,to))
+        else:
+            self.graph[fr] = []
+
+    def BFS(self,s:int) -> None:
+        stack = []
+        visited = [False] * (max(self.graph) + 1)
+        stack.append(s)
+        print(s, end="")
+        visited[s] = True
+        while stack:
+            temp = stack.pop(0)
+            for x in self.graph[temp]:
+                if visited[x] == False:
+                    print(x, end="")
+                    visited[x] = True
+                    stack.append(x)
+        return 
+                
+
+    def DFS(self,s:int) -> None:
+        visited = [False] * (max(self.graph) + 1)
+        def helper(vertex:int, visited:list):
+            visited[vertex] = True
+            print(vertex, end="")
+            for i in self.graph[vertex]:
+                if visited[i] == False:
+                    helper(i, visited)
+        helper(s,visited)
+        return
+    
+    
+    def Reverse(self):
+        reverse = Digraph()
+        for i in self.graph:
+            for w in self.graph[i]:
+                if w not in reverse.graph:
+                    reverse.graph[w] = []
+                reverse.e.append((w,i))
+                reverse.graph[w].append(i)
+        return reverse
+    
+class DepthFirstOrder:
+    def __init__(self, g:Digraph) -> None:
+        self.grap = g.graph
+        self.pre = deque()
+        self.post = deque()
+        self.reversepost = deque()
+        visited = [False] * (max(g.graph) + 1)
+        for i in self.grap:
+            if visited[i] == False:
+                self._dfs(i, visited)
+
+    def _dfs(self,v:int, visited:list):
+        self.pre.appendleft(v)
+        visited[v] = True
+        for i in self.grap[v]:
+            if visited[i] == False:
+                self._dfs(i, visited)
+        self.post.appendleft(v)
+        self.reversepost.append(v)
+
+    @property
+    def PreOrder(self):
+        return self.pre
+    
+    @property
+    def PostOrder(self):
+        return self.post
+    
+    @property
+    def ReversePostOrder(self):
+        return self.reversepost
+    
+        
+if __name__ == '__main__':
+ 
+    # Create a graph given in
+    # the above diagram
+    g = Digraph()
+    g.addEdge(0, 5)
+    g.addEdge(0, 1)
+    g.addEdge(0, 6)
+    g.addEdge(1,None)
+    g.addEdge(2, 0)
+    g.addEdge(2, 3)
+    g.addEdge(3, 5)
+    g.addEdge(4, None)
+    g.addEdge(5, 4)
+    g.addEdge(6, 4)
+    g.addEdge(6, 9)
+    g.addEdge(7, 6)
+    g.addEdge(8, 7)
+    g.addEdge(9, 11)
+    g.addEdge(9, 12)
+    g.addEdge(9, 10)
+    g.addEdge(10, None)
+    g.addEdge(11, 12)
+    g.addEdge(12, None)
+
+ 
+    print("Following is Breadth First Traversal"
+          " (starting from vertex 2)")
+    dc = DepthFirstOrder(g)
+    print(dc.pre)
+    print(dc.post)
+    print(dc.reversepost)
