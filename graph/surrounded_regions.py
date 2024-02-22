56e3ea5596d688a033d28d74e16119fc74c1a039
ajangid007 <anuradha.jangid@gmail.com>
2024-02-21 20:03:18 -0800

    optimized solution

--- graph/surrounded_regions.py
+++ graph/surrounded_regions.py
@@ -5,28 +5,38 @@ class Solution(object):
         :type board: List[List[str]]
         :rtype: None Do not return anything, modify board in-place instead.
         """
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                temp = []
-                if board[i][j] != "X":
-                    Solution.dfs(i,j,board,temp)
-                    if len(temp)> 1:
-                        for (i,j) in temp:
-                            board[i][j] = "X"
+        rows = len(board)
+        cols = len(board[0])
+        def dfs(row,col):
+            if 0<=row<rows and 0<=col<cols and board[row][col] == "O":
+                board[row][col] = "V"
+                dfs(row,col+1)
+                dfs(row,col-1)
+                dfs(row+1,col) 
+                dfs(row-1,col)
+        for i in range(rows):
+            dfs(i,0)
+            dfs(i,cols-1)
+        for j in range(cols):
+            dfs(0,j)
+            dfs(rows-1,j)
+        
+        for i in range(rows):
+            for j in range(cols):
+                if board[i][j] == "V":
+                    board[i][j] = "O"
+                else:
+                    board[i][j] = "X"
         return board
-    @staticmethod
-    def dfs(row, col, grid, path):
-        if (row,col) in path:
-            return
-        if row < 0 or row > len(grid)-1 or col < 0 or col > len(grid[0])-1 or grid[row][col] in ["X"]: 
-            return
-        path.append((row,col))
-        Solution.dfs(row,col+1,grid,path)
-        Solution.dfs(row,col-1,grid,path)
-        Solution.dfs(row+1,col,grid,path) 
-        Solution.dfs(row-1,col,grid,path)
+
 
 board = [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]
 assert Solution().solve(board) == [["X","X","X","X"],["X","X","X","X"],["X","X","X","X"],["X","O","X","X"]]
 board = [["X"]]
 assert Solution().solve(board) == [["X"]]
+board = [["O","O"],["O","O"]]
+assert Solution().solve(board) == [["O","O"],["O","O"]]
+board = [["O","O","O"],["O","O","O"],["O","O","O"]]
+assert Solution().solve(board) == [["O","O","O"],["O","O","O"],["O","O","O"]]
+board = [["X","O","O","X","X","X","O","X","O","O"],["X","O","X","X","X","X","X","X","X","X"],["X","X","X","X","O","X","X","X","X","X"],["X","O","X","X","X","O","X","X","X","O"],["O","X","X","X","O","X","O","X","O","X"],["X","X","O","X","X","O","O","X","X","X"],["O","X","X","O","O","X","O","X","X","O"],["O","X","X","X","X","X","O","X","X","X"],["X","O","O","X","X","O","X","X","O","O"],["X","X","X","O","O","X","O","X","X","O"]]
+assert Solution().solve(board) == [["X","O","O","X","X","X","O","X","O","O"],["X","O","X","X","X","X","X","X","X","X"],["X","X","X","X","X","X","X","X","X","X"],["X","X","X","X","X","X","X","X","X","O"],["O","X","X","X","X","X","X","X","X","X"],["X","X","X","X","X","X","X","X","X","X"],["O","X","X","X","X","X","X","X","X","O"],["O","X","X","X","X","X","X","X","X","X"],["X","X","X","X","X","X","X","X","O","O"],["X","X","X","O","O","X","O","X","X","O"]]
