e921cc8ff9b65ebaa5df7613d94aea580681f521
ajangid007 <anuradha.jangid@gmail.com>
2024-04-23 22:26:39 -0700

    updates

--- array-hashing/group_anagram.py
+++ array-hashing/group_anagram.py
@@ -4,11 +4,11 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         groups = {}
         for ana in strs:
-            strhash = hash(str(sorted(ana)))
-            if strhash in groups.keys():
-                groups[strhash].append(ana)
+            sortedana = sorted(ana)
+            if sortedana in groups.keys():
+                groups[sortedana].append(ana)
             else:
-                groups[strhash] = [ana]
+                groups[sortedana] = [ana]
         return list(groups.values())
 
 assert (Solution().groupAnagrams(["eat","tea","tan","ate","nat","bat"])) == sorted([["bat"],["nat","tan"],["ate","eat","tea"]])
